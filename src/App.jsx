5ac75874a7501cf5ff34066caa3c4fbf22a47f2b
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2023-10-03 09:17:42 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/about" element={<AboutPage />} />
                     <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/cats" element={<Cats />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </main>
